21526ef92032f37c70c8d94fd87f1c3a64900fbc
Kan <katookas.1990@gmail.com>
2021-02-26 12:56:56 +0700

    Lab3,4

---
+++ src/app/counting/counting.component.ts
@@ -0,0 +1,15 @@
+import { sharedStylesheetJitUrl } from '@angular/compiler';
+import { Component, Input } from '@angular/core';
+@Component({
+selector: 'app-counting',
+templateUrl: './counting.component.html',
+styleUrls: ['./counting.component.css']
+})
+export class CountingComponent {
+@Input()
+count = 0;
+constructor() {}
+countNumber() {
+    this.count = this.count + 1;
+}
+}
